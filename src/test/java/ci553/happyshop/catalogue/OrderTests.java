5b522d3e42e41fba499212771be3f0f002621508
jessm <jessmaybryantt@gmail.com>
2026-01-03 12:04:38 +0000

    Created order tests

---
+++ src/test/java/ci553/happyshop/catalogue/OrderTests.java
@@ -0,0 +1,24 @@
+package ci553.happyshop.catalogue;
+import java.util.ArrayList;
+import ci553.happyshop.orderManagement.OrderState;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTests {
+
+
+    @Test
+    void OrderConstructor(){
+        int orderID = 5;
+        OrderState state = OrderState.Collected;
+        String orderedTime = "000";
+        ArrayList<Product> products = new ArrayList<>();
+        Order myOrder = new Order(orderID, state, orderedTime, products);
+        assertNotNull(myOrder);
+        assertEquals(myOrder.getOrderId(), orderID);
+        assertEquals(myOrder.getState(), state);
+        assertEquals(myOrder.getOrderedDateTime(), orderedTime);
+        assertEquals(myOrder.getProductList(), products);
+    }
+}
