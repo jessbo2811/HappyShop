b884d8e03985178e828948f28ea3bbce064598d2
jessm <jessmaybryantt@gmail.com>
2026-01-03 11:49:32 +0000

    Created sample test class with simple test just to get tests working

---
+++ src/test/java/ci553/happyshop/SampleTest.java
@@ -0,0 +1,13 @@
+package ci553.happyshop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest
+ {
+
+    @Test
+    void simpleTest() {
+        assertEquals(2, (1 + 1));
+    }
+}
